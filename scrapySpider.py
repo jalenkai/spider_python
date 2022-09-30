f3621c17c3acc4e575cc27d0fdc264f6ced86d4a
jalenkai <jalenshiu@gmail.com>
2022-09-30 11:39:16 +0800

    Update scrapySpider.py

--- scrapySpider.py
+++ scrapySpider.py
@@ -3,7 +3,7 @@
 import scrapy
 from bs4 import BeautifulSoup
 #from selenium import webdriver
-
+import os
 
 class proxySpider(scrapy.Spider):
     #爬蟲名字
@@ -16,12 +16,32 @@ class proxySpider(scrapy.Spider):
     #相當於response = urllib.request.urlopen(urls)
     def parse(self, response):
         soup = BeautifulSoup(response.text, 'lxml')
+        os.system("cls") #清除畫面
         trs = soup.find_all("table" , class_ ='table table-striped table-bordered')
         for tr in trs:
             tds = tr.select("td")
             if len(tds) > 6:
                 print(tds)
 
+    # def parse_item(self, response):
+        #這裡的item 是用一個dict 字典存取數據
+        # item = {}
+        # div_list = response.xpath('//div[@class="bookslist"]/ul/li/div')
+        # for div in div_list:
+            #data-original 表示圖片是載入，不能擷取src属性
+            # src = div.xpath('./div/a/img/@data-original').extract_first()
+            # name = div.xpath('./div/a/img/@alt').extract_first()
+            # author = div.xpath('./p[1]/a[1]/text()|./p[1]/text()').extract_first()
+            # url = div.xpath('./div/a/@href').extract_first()
+            # url = "https://www.dushu.com" + url
+            # yield scrapy.Request(url=url, callback=self.parse_second, meta={'name': name, 'src': src, 'author': author})
+
+    # def parse_second(self, response):
+        # price = response.xpath('//div[@class="book-details"]//span/text()').get()
+        # name = response.meta['name']
+        # src = response.meta['src']
+        # author = response.meta['author']
+        # yield DushuItem(src=src, name=name, author=author, price=price)
 
 # if __name__ == '__main__':
 #     chrome_options = webdriver.ChromeOptions()
